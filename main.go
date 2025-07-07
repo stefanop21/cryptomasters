da47d895a55c7e8de19f711d49969f6a9a738102
Stefano Palomino <stefanop21@outlook.es>
2025-07-06 20:29:08 -0500

    Refactor main function to retrieve rates for multiple currencies concurrently

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/stefanop21/cryptomasters/api"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func(currencyCode string) {
+			getRate(currencyCode)
+			wg.Done()
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getRate(currency string) {
+	rate, err := api.GetRate(currency)
 	if err != nil {
 		fmt.Println(err)
 	}
