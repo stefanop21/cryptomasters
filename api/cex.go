50f80f275ac008199ad505782d47023d585ca9be
Stefano Palomino <stefanop21@outlook.es>
2025-07-06 20:10:11 -0500

    Add CEX API integration with response handling and rate struct

---
+++ api/cex.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/stefanop21/cryptomasters/datatypes"
+)
+
+const apiUrl = "https://cex.io/api/ticker/%s/USD"
+
+func GetRate(currency string) (*datatypes.Rate, error) {
+	upCurrency := strings.ToUpper(currency)
+	res, err := http.Get(fmt.Sprintf(apiUrl, upCurrency))
+
+	if err != nil {
+		return nil, err
+	}
+	var response CEXResponse
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(bodyBytes, &response)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, fmt.Errorf("status code received: %v", res.StatusCode)
+	}
+	rate := datatypes.Rate{Currency: currency, Price: response.Bid}
+	return &rate, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/stefanop21/cryptomasters
+
+go 1.24.4
